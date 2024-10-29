fdf4c5f6607d432c2a3e8cddde1092d4aa47596d
shihyingru <shihyingru@gmail.com>
2024-10-29 18:11:04 +0800

    [Add] Artwork 2

---
+++ 241029_artwork_2.js
@@ -0,0 +1,18 @@
+/*
+ * Reference Artwork
+ * Vera Molnár Mouvement, 1959
+ * 
+ * Reference Artwork Url
+ * https://ropac.net/artists/231-vera-molnar/works/12824-vera-molnar-mouvement-1959/
+ *
+ */
+function setup() {
+    //TODO("Painting Frame with Bleeding Util(+ Painting Shadow)")
+    //TODO("Save Img/GIF Util")
+
+    // createCanvas();
+}
+
+function draw() {
+
+}
